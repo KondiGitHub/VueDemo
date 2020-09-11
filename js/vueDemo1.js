b105c5cc01cb1cea7a9a2bb5694ba0df3c1f2f49
Venkat <haichary@yahoo.co.in>
2020-09-11 14:55:28 -0400

    Initial commit

---
+++ js/vueDemo1.js
@@ -0,0 +1,15 @@
+
+createVueDemo = function () {
+    new Vue({
+        el: '#app',
+        data: {
+            title: 'Hello World'
+        },
+        methods: {
+            changeTitle : function (event) {
+                this.title= event.target.value;
+            }
+        }
+    });
+}
+
