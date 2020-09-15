d8cbd5eec0500d757090bc4d79b3fabe5fd50430
Venkat <haichary@yahoo.co.in>
2020-09-15 15:26:47 -0400

    some more events

--- js/event.js
+++ js/event.js
@@ -2,11 +2,19 @@ createVueDemo = function () {
     new Vue({
         el: '#app',
         data: {
-           counter: 0
+           counter: 0,
+            x:0,
+            y:0
         },
         methods: {
-            increase: function () {
-                this.counter++
+            // to pass value as argument and receive as event
+            increase: function (step, event) {
+                console.log("event value::"+event.target.value)
+                this.counter+=step
+            },
+            updateCoordinates: function (event) {
+                this.x= event.clientX;
+                this.y =event.clientY
             }
         }
     });
